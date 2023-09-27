4737f1d0e32b4d20da760932f72c4f00c52e2286
yayavin <yayavin>
2023-09-27 18:22:50 -0500

    Added tests for CountingSort.

--- src/main/java/org/example/sorting/CountingSort.java
+++ src/main/java/org/example/sorting/CountingSort.java
@@ -3,15 +3,16 @@ package org.example.sorting;
 import java.util.Arrays;
 import java.util.Comparator;
 
+import static org.example.sorting.exception.ArrayContainsNullValuesException.checkArrayForNullValues;
+
 public class CountingSort {
 
-    //TODO: Make tests instead of main() method
-    public static void main(String[] args) {
-        Integer[] nonSortedList = {5, 4, 3, 2, 1, 0, -1, -2, -3, -4, -5};
-        System.out.println(Arrays.deepToString(sort(nonSortedList)));
-    }
-
     public static Integer[] sort(Integer[] nonSortedList) {
+        checkArrayForNullValues(nonSortedList);
+        if (isEmpty(nonSortedList)) {
+            return nonSortedList;
+        }
+
         //TODO: Think how could be possible to optimize memory (maybe binary tree can help us or other structure,
         // but will it be optimal by time?)
         Integer min = Arrays.stream(nonSortedList)
@@ -37,6 +38,10 @@ public class CountingSort {
             }
         }
         return sortedList;
+    }
+
+    private static boolean isEmpty(Integer[] nonSortedList) {
+        return nonSortedList.length == 0;
     }
 
 }
